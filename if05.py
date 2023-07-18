4fd6c563e227aeff20b0fa63293fcd2b18c85d46
asrorbobomurodov <mr.aceonejr777@gmail.com>
2023-07-18 15:13:10 +0500

    done by @asror

--- if05.py
+++ if05.py
@@ -5,5 +5,31 @@ def main(n):
         n: Five-digit number.
     Returns:
         int: return answer.
-    """
-    return
+    """ #54673
+    n1 = n%10 #3
+    n = n//10 #5467
+
+    n2 = n%10 #7
+    n = n//10 #546
+
+    n3 = n%10 #6
+    n = n//10 #54
+
+    n4 = n%10 #4
+    n = n//10 #5
+
+    n5 = n
+
+    max = n1
+    if n2>max:
+        max = n2
+    if n3>max:
+        max = n3
+    if n4>max:
+        max = n4
+    if n5>max:
+        max = n5
+
+    return max
+print(main(65489))
+print(main(12345))
--- if06.py
+++ if06.py
@@ -6,4 +6,39 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    n1 = n%10
+    n //= 10
+
+    n2 = n%10
+    n //= 10
+
+    n3 = n%10
+    n //= 10
+
+    n4 = n%10 
+    n //=10
+
+    n5 = n
+
+    max = n1
+    if n2>max:
+        max = n2
+    if n3>max:
+        max = n3
+    if n4>max:
+        max = n4
+    if n5>max:
+        max = n5
+    
+    if n1==max:
+        return 1
+    if n2==max:
+        return 2
+    if n3==max:
+        return 3
+    if n4==max:
+        return 4
+    if n5==max:
+        return 5
+print(main(28561))
+print(main(85648))
